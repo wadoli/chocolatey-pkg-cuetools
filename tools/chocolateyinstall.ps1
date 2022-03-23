fe750b89ab24bb758d267ca02235ffe9a0f0fea0
Oliver Waddell <wadoli@users.noreply.github.com>
2022-03-24 00:59:48 +0100

    Updated package to v. 2.2.1 of cuetools

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = ''
 
-#Based on NO DETECTION IN PRO
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'cuetools*'
   fileType      = 'zip'
-  url           = "https://github.com/gchudov/cuetools.net/releases/download/v2.1.9/CUETools_2.1.9.zip"  #download URL, HTTPS preferrred
-  checksum      = '28B065547D779CFCA1CF33F9548C3AACCBA6BF2BAC3F973FEBA789088D2A6CCE'
+  silentArgs    = "/S"
+  validExitCodes= @(0) #please insert other valid exit codes here
+  url           = "https://github.com/gchudov/cuetools.net/releases/download/v2.2.1/CUETools_2.2.1.zip"  #download URL, HTTPS preferrred
+  checksum      = 'c95051bc1d01639aa9d8a7072b61f71ac3c73f07fab244c33b4aff48d0c640a3'
   checksumType  = 'sha256'
   destination   = $toolsDir
-  specificFolder= 'CUETools_2.1.9'
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-## See https://chocolatey.org/docs/helpers-reference
+## See https://docs.chocolatey.org/en-us/create/functions/
+
