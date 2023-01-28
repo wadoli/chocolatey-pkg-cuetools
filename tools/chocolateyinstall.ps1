e5e6d665a0e287573e4678a3d4c516423f0e2209
wadoli <wadoli@users.noreply.github.com>
2023-01-28 22:15:13 +0100

    v2.2.3 of cuetools plus added back v2.2.1 nupkg

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   fileType      = 'zip'
   silentArgs    = "/S"
   validExitCodes= @(0) #please insert other valid exit codes here
-  url           = "https://github.com/gchudov/cuetools.net/releases/download/v2.2.2/CUETools_2.2.2.zip"  #download URL, HTTPS preferrred
-  checksum      = 'b56914afc067a7937d8c905f0910f252d18d6bd8172a0a733314a318fa208aa9'
+  url           = "https://github.com/gchudov/cuetools.net/releases/download/v2.2.3/CUETools_2.2.3.zip"  #download URL, HTTPS preferrred
+  checksum      = '017484aab76040737713a9a638eb696db63df0a5f792973744b36a41bd60fb98'
   checksumType  = 'sha256'
   destination   = $toolsDir
 }
